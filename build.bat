53e580ac12cecbc5bd3a7fe7f7744c01c61e2ce4
Jaime Barillas <jaimeenrique97@gmail.com>
2019-02-22 19:07:35 -0500

    Building in NetBeans calls build.bat to generate css files and copy various files to the tomcat server.

---
+++ build.bat
@@ -0,0 +1,26 @@
+@ECHO OFF
+SETLOCAL
+rem TODO: Add servlet classes to WEB-INF\web.xml file?
+SET "tomcatAppDir=C:\Users\jaime\Documents\DC-Y2-S2\webe\tomcat8\webapps\webd4201.barillasj\"
+SET "projectDir=C:\Users\jaime\Documents\DC-Y2-S2\webe\website\"
+SET "classFilesDir=%projectDir%build\classes\webd4201\barillasj\"
+
+ECHO.
+ECHO Compiling css styles in %classFilesDir%webpages\css
+rem NOTE: sass is itself another batch file so it must be called with call
+CALL sass --no-source-map %classFilesDir%webpages\css\:%classFilesDir%webpages\css\
+
+ECHO Copying css styles and images in %classFilesDir%webpages\{css,images}\ to %tomcatAppDir%{css,images}
+rem 1>NUL stops any output to stdout from showing up in the console, 2>NUL works for stderr
+COPY /Y %classFilesDir%webpages\css\*.css %tomcatAppDir%css 1>NUL 2>NUL
+COPY /Y %classFilesDir%webpages\images\*.png %tomcatAppDir%images 1>NUL 2>NUL
+COPY /Y %classFilesDir%webpages\images\*.jpg %tomcatAppDir%images 1>NUL 2>NUL
+
+ECHO Copying jsp pages in %classFilesDir%webpages to %tomcatAppDir%
+COPY /Y %classFilesDir%webpages\*.jsp %tomcatAppDir% 1>NUL
+
+ECHO Copying class files in %classFilesDir% and subdirectories to %tomcatAppDir%WEB-INF\classes
+rem Copy all the .class files to the tomcat webapp folder under the WEB-INF\classes\ subfolder
+XCOPY /Y /S /C /Q %classFilesDir%*.class %tomcatAppDir%WEB-INF\classes\ 1>NUL
+
+ENDLOCAL
